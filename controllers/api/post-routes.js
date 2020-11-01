bd04534c927fc7e226758b7842f73d6115268d0a
Ida <67982739+igk1024@users.noreply.github.com>
2020-11-01 12:16:21 -0600

    updating

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,162 @@
+const router = require('express').Router();
+const {
+  Post,
+  User,
+  Vote,
+  Comment
+} = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+// get all users
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
+        'blog_text'
+      ],
+      order: [
+        ['created_at', 'DESC']
+      ],
+      include: [{
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+    .then(dbPostData => {
+
+        const posts = dbPostData.map(post => post.get({
+        plain: true
+      }));
+
+      res.render('homepage', {
+        posts
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+});
+
+router.get('/:id', (req, res) => {
+  Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
+        'blog_text'
+      ],
+      include: [{
+        model: User,
+        attributes: ['username']
+      }]
+    })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: 'No post found with this id'
+        });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+  Post.create({
+      title: req.body.title,
+      blog_text: req.body.blog_text,
+      user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put('/upvote', withAuth, (req, res) => {
+
+    if (req.session) {
+
+    Post.upvote({
+        ...req.body,
+        user_id: req.session.user_id
+      }, {
+        Vote,
+        Comment,
+        User
+      })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+});
+
+router.put('/:id', withAuth, (req, res) => {
+  Post.update({
+      title: req.body.title,
+      blog_text: req.body.blog_text
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: 'No post found with this id'
+        });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+  Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Comment]
+    })
+    .then(post => {
+      post.comments.forEach(comment => {
+        comment.destroy();
+      })
+      post.destroy();
+      res.end();
+    })
+});
+
+
+module.exports = router;
---
+++ public/javascript/delete-post.js
@@ -0,0 +1,16 @@
+async function deleteFormHandler(event) {
+  event.preventDefault();
+
+  await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+
+};
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
